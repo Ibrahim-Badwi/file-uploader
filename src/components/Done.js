7357d9969e6d619bfb35bfb7a0a6c0992853fed5
Ibrahim Badwi <ib.a.badwi@gmail.com>
2021-01-11 18:49:22 +0300

    refactor (done.js) component

--- src/components/Done.js
+++ src/components/Done.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import tickMArk from '../../assets/tick.svg';
+
+const Result = () => {
+  return (
+    <div className='result'>
+      <img className='tick-icon' src={tickMArk} alt='tick-mark' />
+      <p>Uploaded Successfully!</p>
+      <img className='uploaded-img' src={require('../../assets/mern-600x200.jpg')} />
+      <div>
+        <input type='text' value='https://www.flaticon.com/free-icon/tick_87932?search-type=icons&k=1610365571354&related_id=87932&origin=search' />
+        <button>Copy Link</button>
+      </div>
+    </div>
+  );
+};
 
 const Done = ({ show }) => {
   return (
     <>
-      {show
-        ? <p>Done</p>
-        : null
-      }
+      { show ? <Result /> : null }
     </>
   );
 };

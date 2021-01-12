947c2f0aa56b4fb978dccf208a38fb712923be06
Ibrahim Badwi <ib.a.badwi@gmail.com>
2021-01-12 20:39:18 +0300

    refactor (SelectFile.js)

--- src/components/SelectFile.js
+++ src/components/SelectFile.js
@@ -1,27 +1,20 @@
 import React from 'react';
 import svg from '../../assets/image.svg';
 
-const Header = () => {
-  return (
-    <div>
-      <h1>Upload your image</h1>
-      <p className="header-p">File should be Jpeg, Png,...</p>
-    </div>
-  );
-};
 
-const DragAndDrop = () => {
-  return (
-    <div className='drag-container'>
-      <img src={svg} alt='svg1' />
-      <p>Drag & Drop your image here</p>
-    </div>
-  );
-};
-
-const File = ({ onSelect }) => {
+const SelectFile = ({ onSelect }) => {
   return (
     <>
+      <div>
+        <h1>Upload your image</h1>
+        <p className="header-p">File should be Jpeg, Png,...</p>
+      </div>
+
+      <div className='drag-container'>
+        <img src={svg} alt='svg1' />
+        <p>Drag & Drop your image here</p>
+      </div>
+
       <p className='sep'>Or</p>
       <div className='button-wrap'>
         <label className='new-button' htmlFor='upload'>Choose a file</label>
@@ -32,16 +25,6 @@ const File = ({ onSelect }) => {
           onChange={onSelect}
         />
       </div>
-    </>
-  );
-};
-
-const SelectFile = ({ onSelect }) => {
-  return (
-    <>
-      <Header />
-      <DragAndDrop />
-      <File onSelect={onSelect} />
     </>
   );
 };

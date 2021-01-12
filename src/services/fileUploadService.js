f34346f5b02e74d4076ae013edc23506af5a0e35
Ibrahim Badwi <ib.a.badwi@gmail.com>
2021-01-12 17:23:01 +0300

    feat (fileUploadService) create upload service

---
+++ src/services/fileUploadService.js
@@ -0,0 +1,27 @@
+import http from './http-common';
+
+const uploadFile = file => {
+  let data = new FormData();
+
+  data.append('file', file);
+
+  return http.post('/upload', data, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    }
+  });
+};
+
+const getFiles = () => {
+  return http.get('/files');
+};
+
+const getFile = (id) => {
+  return http.get(`/files/${id}`);
+};
+
+export default {
+  uploadFile,
+  getFiles,
+  getFile
+};

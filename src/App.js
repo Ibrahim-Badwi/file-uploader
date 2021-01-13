7359ed37147647322221e1c589fb90f49cf6f685
Ibrahim Badwi <ib.a.badwi@gmail.com>
2021-01-13 23:19:23 +0300

    reafcator (App.js)

--- src/App.js
+++ src/App.js
@@ -4,32 +4,44 @@ import SelectFile from './components/SelectFile';
 import Upload from './components/Upload';
 import Done from './components/Done';
 
+import uploadService from './services/fileUploadService';
+
 
 const App = () => {
-  const [file, setFile] = useState('');
+  const [selectedFile, setSelectedFile] = useState('');
   const [uploading, setUploading] = useState(false);
-  const [mina, setMain] = useState(true);
   const [success, setSuccess] = useState(false);
+  const [url, setUrl] = useState('');
 
   const  klass = uploading ? 'sm-container' : 'container';
 
-  const handleUpload = (event) => {
-    console.log(event.target.files);
-    setFile(event.target.files);
+  const handleUpload = file => {
+    console.log(file);
+    setSelectedFile(file);
     setUploading(true);
-    setTimeout(() => {
-      setUploading(false);
-    }, 100000000000000);
+
+    uploadService
+      .uploadFile(setSelectedFile)
+      .then(response => {
+        console.log(response);
+        setUploading(false);
+        // setUrl(response.url);
+        setUrl('../../assets/mern-600x200.jpg');
+        setSuccess(true);
+      });
+  };
+
+  const upload = () => {
+
   };
 
   return (
     <div className={klass}>
-      {!uploading
+      {!uploading && !success
         ? <SelectFile onSelect={handleUpload} show={uploading} />
         : <Upload show={uploading} file/>
       }
-      {success && <Done show={uploading} />}
-      {/* <Done show={!uploading} /> */}
+      {success && <Done url={url} />}
     </div>
   );
 };

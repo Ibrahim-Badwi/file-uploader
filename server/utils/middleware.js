3bbaeb2838dfc73e80d09a9875c9e698a50aee5b
Ibrahim Badwi <ib.a.badwi@gmail.com>
2021-01-15 23:18:34 +0300

    feat (server/utils) create middleware module

---
+++ server/utils/middleware.js
@@ -0,0 +1,37 @@
+const uploadFile = require('../middleware/upload');
+const logger = require('../utils/logger');
+
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method);
+  logger.info('Path:  ', request.path);
+  logger.info('Body:  ', request.body);
+  logger.info('---');
+  next();
+};
+const unknownEndpoint = (request, response) => {
+  return response.status(404).send({ error: 'unknown endpoint' });
+};
+
+const errorHandler = (error, request, response, next) => {
+  if(error.code === 'ENOENT') {
+    return response.status(500).send({
+      message: `Could not download the file: ${request.id}`,
+    });
+  } else if (error.code === 'LIMIT_FILE_SIZE') {
+    return response.status(500).send({
+      message: 'File size cannot be larger than 2MB!',
+    });
+  } else {
+    return response.status(500).send({
+      message: `Could not upload the file: ${error}`,
+    });
+  }
+  // next(error);
+};
+
+module.exports = {
+  requestLogger,
+  uploadFile,
+  unknownEndpoint,
+  errorHandler
+};

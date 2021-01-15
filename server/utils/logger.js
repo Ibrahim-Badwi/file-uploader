8f9e63db09e77fe6596a6bbab65ef2950a97196f
Ibrahim Badwi <ib.a.badwi@gmail.com>
2021-01-15 23:18:10 +0300

    feat (server/utils) create logger module

---
+++ server/utils/logger.js
@@ -0,0 +1,13 @@
+// logger module usefull when writting logs to file or external logging service
+const info = (...params) => {
+  console.log(...params);
+};
+
+const error = (...params) => {
+  console.error(...params)
+};
+
+module.exports = {
+  info,
+  error
+};

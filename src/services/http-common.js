945addf287a105c4601a09c4d99264351aea0cb7
Ibrahim Badwi <ib.a.badwi@gmail.com>
2021-01-17 16:57:38 +0300

    connect to server using BACKEND_URL from webpack

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3001/api/files';
-// const BASE_URL = '/api/files';
 
 export default axios.create({
-  baseURL: BASE_URL,
+  baseURL: BACKEND_URL,
   headers: {
     'Content-type': 'application/json'
   }
